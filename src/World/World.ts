b89309813db3cb9f8cc2c2495e4c12fdfe7de5b2
Ngọc Đinh <ngokhoghat@gmail.com>
2022-01-14 07:29:18 +0000

    update

--- src/World/World.ts
+++ src/World/World.ts
@@ -3,6 +3,7 @@ import { Cube } from "./components/Cube";
 import { Light } from "./components/Light";
 import { MeshGroup } from "./components/MeshGroup";
 import { Scene } from "./components/Scene";
+import { Train } from "./entitys/train/train";
 import { Controls } from "./systems/Controls";
 import { Loop } from "./systems/Loop";
 import { Renderer } from "./systems/Renderer";
@@ -24,15 +25,16 @@ export class World {
         this.controls = new Controls().createControls(this.camera, this.renderer.domElement);
         container.append(this.renderer.domElement);
 
+        const train = new Train();
         const cube = new Cube().createCube();
         const meshGroup = new MeshGroup().createMeshGroup();
         const { light, ambientLight } = new Light().createLights();
 
-        this.loop.updatables.push(cube);
         this.loop.updatables.push(meshGroup);
+        this.loop.updatables.push(train);
         this.loop.updatables.push(this.controls);
 
-        this.scene.add(light, ambientLight, meshGroup);
+        this.scene.add(light, ambientLight, train);
         this.controls.target.copy(cube.position);
 
         const resizer = new Resizer(container, this.camera, this.renderer);
---
+++ src/World/entitys/train/train.ts
@@ -0,0 +1,31 @@
+import { Group, MathUtils } from 'three';
+import { createMeshes } from './meshes';
+
+const wheelSpeed = MathUtils.degToRad(24);
+
+export class Train extends Group {
+    private meshes;
+
+    constructor() {
+        super();
+
+        this.meshes = createMeshes();
+        this.add(
+            this.meshes.nose,
+            this.meshes.cabin,
+            this.meshes.chimney,
+            this.meshes.smallWheelRear,
+            this.meshes.smallWheelCenter,
+            this.meshes.smallWheelFront,
+            this.meshes.bigWheel
+        );
+    }
+
+
+    tick(delta: number) {
+        this.meshes.bigWheel.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelRear.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelCenter.rotation.y += wheelSpeed * delta;
+        this.meshes.smallWheelFront.rotation.y += wheelSpeed * delta;
+    }
+}
